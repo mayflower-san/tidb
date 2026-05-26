9bab9442acedca6316d3fc8b49d5732dc87c1667
bb7133 <bb7133@gmail.com>
2026-05-26 14:26:10 -0700

    executor: support _tidb_rowid in INSERT RETURNING

--- pkg/executor/builder.go
+++ pkg/executor/builder.go
@@ -1090,10 +1090,11 @@ func (b *executorBuilder) buildInsert(v *physicalop.Insert) exec.Executor {
 		return b.buildReplace(ivs)
 	}
 	insert := &InsertExec{
-		InsertValues:    ivs,
-		OnDuplicate:     append(v.OnDuplicate, v.GenCols.OnDuplicates...),
-		returningExprs:  v.Returning,
-		returningSchema: v.ReturningSchema,
+		InsertValues:             ivs,
+		OnDuplicate:              append(v.OnDuplicate, v.GenCols.OnDuplicates...),
+		returningExprs:           v.Returning,
+		returningSchema:          v.ReturningSchema,
+		returningNeedExtraHandle: v.NeedExtraHandleReturning,
 	}
 	return insert
 }
